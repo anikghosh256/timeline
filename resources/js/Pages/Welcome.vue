dee1a669283f16ab2232e115968c9c985f4ae2ef
Anik Ghosh <anikghosh46.bd@gmail.com>
2022-10-09 19:08:27 +0600

    Welcome page update.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,25 +12,26 @@ defineProps({
 <template>
     <Head title="Welcome" />
 
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.user" :href="route('timeline')" class="text-sm text-gray-700 underline">
-                TimeLine
-            </Link>
+    <div class="relative flex justify-center min-h-screen bg-gray-100 dark:bg-gray-900 items-center sm:pt-0">
 
-            <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 underline">
-                    Log in
-                </Link>
-
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
-                    Register
-                </Link>
-            </template>
-        </div>
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <h1 class="font-extrabold text-6xl">TimeLine</h1>
+            <div v-if="canLogin" class="px-6 py-4 sm:block text-center">
+                <Link v-if="$page.props.user" :href="route('timeline')" class="text-sm text-gray-700 underline">
+                    TimeLine
+                </Link>
+
+                <template v-else>
+                    <Link :href="route('login')" class="text-sm text-gray-700 underline">
+                        Log in
+                    </Link>
+
+                    <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                        Register
+                    </Link>
+                </template>
+            </div>
         </div>
     </div>
 </template>
